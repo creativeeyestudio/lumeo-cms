0bd3425bb3d57d6891363bb188c4240d69aecc72
creativeeyestudio <contact@creative-eye.fr>
2025-12-13 22:07:31 +0000

    TextIntro Layout

---
+++ themes/lumeo-theme-base/src/layouts/TextIntro/TextIntro.interface.ts
@@ -0,0 +1,3 @@
+interface TextIntroProps {}
+
+export default TextIntroProps
---
+++ themes/lumeo-theme-base/src/layouts/TextIntro/TextIntro.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TextIntroProps from './TextIntro.interface';
+import * as styles from './TextIntro.module.scss';
+
+const TextIntro: React.FC<TextIntroProps> = () => {
+    return (<></>)
+}
+
+export default TextIntro
