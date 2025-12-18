735c9bfc6a96f900a59c7e2a917038a7d124a607
creativeeyestudio <contact@creative-eye.fr>
2025-12-18 09:01:36 +0100

    #16 - Required link type

--- backend/src/components/LinkComponent.tsx
+++ backend/src/components/LinkComponent.tsx
@@ -9,46 +9,64 @@ const LinkComponent = (imageView: boolean = false): Field[] => [
                 description: 'Configration du lien',
                 fields: [
                     {
-                    name: 'label',
-                    type: 'text',
-                    localized: true,
-                    required: true,
-                },
-                {
-                    name: 'type',
-                    type: 'radio',
-                    options: [
-                        { label: 'Page', value: 'page' },
-                        { label: 'Article', value: 'post' },
-                        { label: 'Lien personnalisé', value: 'external' },
-                    ],
-                    defaultValue: 'page',
-                    required: true,
-                },
-                {
-                    name: 'page',
-                    type: 'relationship',
-                    relationTo: 'pages',
-                    admin: {
-                        condition: (_data, sibling) => sibling.type === 'page',
+                        name: 'label',
+                        type: 'text',
+                        localized: true,
+                        required: true,
                     },
-                },
-                {
-                    name: 'post',
-                    type: 'relationship',
-                    relationTo: 'posts',
-                    admin: {
-                        condition: (_data, sibling) => sibling.type === 'post',
+                    {
+                        name: 'type',
+                        type: 'radio',
+                        options: [
+                            { label: 'Page', value: 'page' },
+                            { label: 'Article', value: 'post' },
+                            { label: 'Lien personnalisé', value: 'external' },
+                        ],
+                        defaultValue: 'page',
+                        required: true,
                     },
-                },
-                {
-                    name: 'url',
-                    type: 'text',
-                    admin: {
-                        condition: (_data, sibling) => sibling.type === 'external',
+                    {
+                        name: 'page',
+                        type: 'relationship',
+                        relationTo: 'pages',
+                        admin: {
+                            condition: (_data, sibling) => sibling.type === 'page',
+                        },
+                        validate: (value, { siblingData }) => {
+                            if (siblingData.type === 'page' && !value) {
+                                return 'Une page est obligatoire'
+                            }
+                            return true
+                        },
                     },
-                }
-            ]
+                    {
+                        name: 'post',
+                        type: 'relationship',
+                        relationTo: 'posts',
+                        admin: {
+                            condition: (_data, sibling) => sibling.type === 'post',
+                        },
+                        validate: (value, { siblingData }) => {
+                            if (siblingData.type === 'post' && !value) {
+                                return 'Un article est obligatoire'
+                            }
+                            return true
+                        },
+                    },
+                    {
+                        name: 'url',
+                        type: 'text',
+                        admin: {
+                            condition: (_data, sibling) => sibling.type === 'external',
+                        },
+                        validate: (value, { siblingData }) => {
+                            if (siblingData.type === 'external' && !value) {
+                                return 'Une URL est obligatoire'
+                            }
+                            return true
+                        },
+                    }
+                ]
             },
             {
                 label: 'Paramètres',
@@ -67,12 +85,10 @@ const LinkComponent = (imageView: boolean = false): Field[] => [
                     type: 'checkbox',
                     label: 'Ouvrir dans un nouvel onglet',
                 }
-            ]
+                ]
             }
         ]
     },
-
-
 ]
 
 export default LinkComponent;
