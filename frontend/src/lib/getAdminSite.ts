632bc6f60afead8a4b44dd617431dd55776351f2
creativeeyestudio <contact@creative-eye.fr>
2025-12-18 18:30:52 +0000

    #16 - Get Admin Data

---
+++ frontend/src/lib/getAdminSite.ts
@@ -0,0 +1,12 @@
+export default async function getAdminSite() {
+  const res = await fetch(`${process.env.PAYLOAD_URL}/api/globals/customization?depth=2&draft=false&locale=fr&trash=false`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch Settings`);
+  }
+
+  const page = await res.json();
+  return page;
+}
